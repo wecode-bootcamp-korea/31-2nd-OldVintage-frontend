53b159c8ba7011668e57a17b47a02e6e1b4b24e6
jay yoon <jaytaildish@gmail.com>
2022-04-15 00:00:21 +0900

    FIX:Slide구현 및 사용하지않는 theme import제거

--- src/Pages/ProductDetail/ProductDetail.js
+++ src/Pages/ProductDetail/ProductDetail.js
@@ -1,7 +1,50 @@
 import React from 'react';
+import SliderBar from './Component/SliderBar/SliderBar';
+import styled from 'styled-components';
 
-const Product = () => {
-  return <div>상세페이지</div>;
+const ProductDetail = () => {
+  return (
+    <TasteWrapper>
+      <FlexRow>
+        <FlexColumn>
+          <TasteQuestion>What does this wine taste like?</TasteQuestion>
+          <SliderBar />
+        </FlexColumn>
+        <TasteSummary>
+          <h2>WINE LOVERS TASTE SUMMARY</h2>
+          <span>
+            The taste profile of "product_name" is based on "reviewer_sumTotal"
+            user reviews
+          </span>
+        </TasteSummary>
+      </FlexRow>
+    </TasteWrapper>
+  );
 };
 
-export default Product;
+export default ProductDetail;
+
+const TasteWrapper = styled.section`
+  ${({ theme }) => theme.flexSet('column', '', 'center')}
+  flex-wrap: wrap;
+`;
+
+const FlexColumn = styled.div`
+  ${({ theme }) => theme.flexSet('column', '', '')}
+`;
+
+const FlexRow = styled.section`
+  ${({ theme }) => theme.flexSet('row', '', '')}
+`;
+
+const TasteQuestion = styled.span`
+  font-size: ${({ theme }) => theme.fontSize.large};
+`;
+
+const TasteSummary = styled.div`
+  margin: 70px 0 0 40px;
+  max-width: 400px;
+  h2 {
+    font-size: 20px;
+  }
+`;
