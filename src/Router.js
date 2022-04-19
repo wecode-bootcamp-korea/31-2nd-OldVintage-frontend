815b0a585a5468519fc0b727883b57a2d38156b6
minsu <cocacolazero024@gmail.com>
2022-04-19 19:04:24 +0900

    MODIFY : Eslint 이슈 수정

--- src/Router.js
+++ src/Router.js
@@ -5,15 +5,19 @@ import Nav from './Component/Nav/Nav';
 import Footer from './Component/Footer/Footer';
 import ProductList from './Pages/ProductList/ProductList';
 import ProductDetail from './Pages/ProductDetail/ProductDetail';
+import LoginModal from './Pages/Login/LoginModal';
+import KakaoLogin from './Pages/Login/KakaoLogin';
 
 const Router = () => {
   return (
     <BrowserRouter>
+      <LoginModal />
       <Nav />
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/products" element={<ProductList />} />
         <Route path="/products/:id" element={<ProductDetail />} />
+        <Route path="/oauth/kakao" element={<KakaoLogin />} />
       </Routes>
       <Footer />
     </BrowserRouter>
--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
 const BASE_URL = 'http://10.58.4.204:8000';
 
-const API = {
+export const API = {
   productList: `${BASE_URL}/products`,
+  KakaoLogin: `${BASE_URL}/users/kakao`,
 };
 
 export default API;
